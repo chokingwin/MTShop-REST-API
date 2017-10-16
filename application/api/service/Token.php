41843bf23327049a492d173ce8bb373781652d70
chokingwin <2205405321@qq.com>
2017-10-16 17:06:25 +0800

    补充上次提交，修改文件目录。

---
+++ application/api/service/Token.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace app\api\service;
+
+
+class Token
+{
+    public static function generateToken()
+    {
+        // 32个字符组成一组随机字符串
+        $randChars = getRandChars(32);
+        // 用三组字符串，进行md5加密
+        $timestamp = $_SERVER['REQUEST_TIME_FLOAT'];
+        // salt 盐
+        $salt = config('security.token_salt');
+        return md5($randChars . $timestamp . $salt);
+    }
+
+    public static function getCurrentTokenVar($key)
+    {
+        // 约定 Token放在HTTP请求的header里
+        $token = Request::instance()->header('token');
+        $vars = Cache::get($token);
+        if (!$vars) {
+            throw new TokenException();
+        } else {
+            if (!is_array($vars)) {
+                $vars = json_decode($vars, true);
+            }
+            if (array_key_exists($key, $vars)) {
+                return $vars[$key];
+            } else {
+                throw new Exception('尝试获取的Token变量并不存在');
+            }
+        }
+    }
+
+    public static function getCurrentUid()
+    {
+        return self::getCurrentTokenVar('uid');
+    }
+
+    public static function getCurrentTokenAllVar()
+    {
+        // 约定 Token放在HTTP请求的header里
+        $token = Request::instance()->header('token');
+        $vars = Cache::get($token);
+        if (!$vars) {
+            throw new TokenException();
+        }
+        if (!is_array($vars)) {
+            $vars = json_decode($vars, true);
+        }
+
+        return $vars;
+    }
+
+    // 用户和CMS管理员都可以访问的权限
+    public static function needPrimaryScope()
+    {
+        $scope = self::getCurrentTokenVar('scope');
+
+        if ($scope) {
+            if ($scope >= ScopeEnum::User) {
+                return true;
+            } else {
+                throw new ForbiddenException();
+            }
+        } else {
+            throw new TokenException();
+        }
+    }
+
+    // 只有用户才能访问的接口权限
+    public static function needExclusiveScope()
+    {
+        $scope = self::getCurrentTokenVar('scope');
+
+        if ($scope) {
+            if ($scope == ScopeEnum::User) {
+                return true;
+            } else {
+                throw new ForbiddenException();
+            }
+        } else {
+            throw new TokenException();
+        }
+    }
+
+    // 只有CMS管理员才能访问的接口权限
+    public static function needSuperScope()
+    {
+        $scope = self::getCurrentTokenVar('scope');
+
+        if ($scope) {
+            if ($scope == ScopeEnum::Super) {
+                return true;
+            } else {
+                throw new ForbiddenException();
+            }
+        } else {
+            throw new TokenException();
+        }
+    }
+
+    public static function isValidOperation($checkedID)
+    {
+        if (!$checkedID) {
+            throw new Exception('检查UID时必须传入一个被检查的UID');
+        }
+        $currentOperationUID = self::getCurrentUid();
+        return $currentOperationUID == $checkedID ? true : false;
+    }
+
+    public static function verifyToken($token)
+    {
+        $exist = Cache::get($token);
+        if ($exist) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
